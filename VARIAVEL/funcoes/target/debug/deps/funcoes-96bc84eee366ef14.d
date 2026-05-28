b6df4619313cf8dcf143f54eef2517970c17daa7
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-05-28 12:55:00 -0300

    Estudando sobre funções

---
+++ VARIAVEL/funcoes/target/debug/deps/funcoes-96bc84eee366ef14.d
@@ -0,0 +1,5 @@
+/home/david/Área de Trabalho/Programação /Rust/trilha_rust/VARIAVEL/funcoes/target/debug/deps/funcoes-96bc84eee366ef14.d: src/main.rs
+
+/home/david/Área de Trabalho/Programação /Rust/trilha_rust/VARIAVEL/funcoes/target/debug/deps/funcoes-96bc84eee366ef14: src/main.rs
+
+src/main.rs:
