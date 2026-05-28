b382e37af15646fc96bacf28bbe7494f487e67b9
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-05-28 08:47:58 -0300

    Fazendo a primeira variavel

---
+++ VARIAVEL/variavel/target/debug/variavel.d
@@ -0,0 +1,1 @@
+/home/david/Área\ de\ Trabalho/Programação\ /Rust/trilha_rust/VARIAVEL/variavel/target/debug/variavel: /home/david/Área\ de\ Trabalho/Programação\ /Rust/trilha_rust/VARIAVEL/variavel/src/main.rs
