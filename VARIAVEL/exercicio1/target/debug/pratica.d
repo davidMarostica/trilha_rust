96eecb854188edea0f21ee762d9a84621af51c23
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-05-28 09:51:56 -0300

    Criando primeiro Exercicio

---
+++ VARIAVEL/exercicio1/target/debug/pratica.d
@@ -0,0 +1,1 @@
+/home/david/Área\ de\ Trabalho/Programação\ /Rust/trilha_rust/VARIAVEL/exercicio1/target/debug/pratica: /home/david/Área\ de\ Trabalho/Programação\ /Rust/trilha_rust/VARIAVEL/exercicio1/src/main.rs
--- VARIAVEL/pratica/target/debug/pratica.d
+++
@@ -1,1 +0,0 @@
-/home/david/Área\ de\ Trabalho/Programação\ /Rust/trilha_rust/VARIAVEL/pratica/target/debug/pratica: /home/david/Área\ de\ Trabalho/Programação\ /Rust/trilha_rust/VARIAVEL/pratica/src/main.rs
