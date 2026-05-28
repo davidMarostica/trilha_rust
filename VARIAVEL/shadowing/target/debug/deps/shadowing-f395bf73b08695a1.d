5c8ca08ea9c70c614aa27f177d219519dd3f04f9
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-05-28 09:05:03 -0300

    Estudando variavel shadowing

---
+++ VARIAVEL/shadowing/target/debug/deps/shadowing-f395bf73b08695a1.d
@@ -0,0 +1,5 @@
+/home/david/Área de Trabalho/Programação /Rust/trilha_rust/VARIAVEL/shadowing/target/debug/deps/shadowing-f395bf73b08695a1.d: src/main.rs
+
+/home/david/Área de Trabalho/Programação /Rust/trilha_rust/VARIAVEL/shadowing/target/debug/deps/shadowing-f395bf73b08695a1: src/main.rs
+
+src/main.rs:
