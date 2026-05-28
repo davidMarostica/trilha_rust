9f6b139295879c8d51d2eaf59cc639d1b036883d
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-05-28 10:00:00 -0300

    acrescentando dia e calculo exato de idade

--- VARIAVEL/exercicio1/src/main.rs
+++ VARIAVEL/exercicio1/src/main.rs
@@ -3,8 +3,23 @@ fn main() {
     Então devo ter o valor da idade da pessoa
     */
     let ano_nascimento: i32 = 1983;
+    let mes_nascimento: i32 = 6;
+    let dia_nascimento: i32 = 14;
+
     let ano_atual: i32 = 2026;
-    let idade: i32 = ano_atual - ano_nascimento;
+    let mes_atual: i32 = 5;
+    let dia_atual: i32 = 15;
+
+    let mut idade: i32 = ano_atual - ano_nascimento;
+    if mes_atual < mes_nascimento {
+        println!("A pessoa ainda não fez aniversário este ano.");
+        idade -= 1;
+    } else if mes_atual == mes_nascimento && dia_atual < dia_nascimento {
+        println!("A pessoa ainda não fez aniversário este ano.");
+        idade -= 1;
+    } else {
+        println!("A pessoa já fez aniversário este ano.");
+    }
 
     println!("A idade da pessoa é: {}", idade);
 }
