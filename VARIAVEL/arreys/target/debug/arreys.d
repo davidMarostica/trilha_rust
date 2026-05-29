c66ae49f98b51e11fa51f4348e1ce5ef25fd2e69
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-05-29 10:07:29 -0300

    Estudando Tuplas e Arreus

---
+++ VARIAVEL/arreys/target/debug/arreys.d
@@ -0,0 +1,1 @@
+/home/david/Área\ de\ Trabalho/Programação\ /Rust/trilha_rust/VARIAVEL/arreys/target/debug/arreys: /home/david/Área\ de\ Trabalho/Programação\ /Rust/trilha_rust/VARIAVEL/arreys/src/main.rs
---
+++ VARIAVEL/arreys/target/debug/deps/arreys-61d9679cf246281e.d
@@ -0,0 +1,5 @@
+/home/david/Área de Trabalho/Programação /Rust/trilha_rust/VARIAVEL/arreys/target/debug/deps/arreys-61d9679cf246281e.d: src/main.rs
+
+/home/david/Área de Trabalho/Programação /Rust/trilha_rust/VARIAVEL/arreys/target/debug/deps/arreys-61d9679cf246281e: src/main.rs
+
+src/main.rs:
