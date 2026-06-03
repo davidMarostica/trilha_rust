b5d63bf5ca71c553fa416a3d32dfacafd000bc6f
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-06-03 10:44:58 -0300

    Remove target do versionamento e adiciona .gitignore global

--- ESTRUTURA/structs/target/debug/deps/structs-69d3a61d0f40bf9b.d
+++
@@ -1,5 +0,0 @@
-/home/david/Área de Trabalho/Programação /Rust/trilha_rust/ESTRUTURA/structs/target/debug/deps/structs-69d3a61d0f40bf9b.d: src/main.rs
-
-/home/david/Área de Trabalho/Programação /Rust/trilha_rust/ESTRUTURA/structs/target/debug/deps/structs-69d3a61d0f40bf9b: src/main.rs
-
-src/main.rs:
