59993d9f8745ec4a0d2317356c60a7ba32a59da3
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-06-12 11:10:11 -0300

    Removendo projeto duplicada

--- GENERICOS/duplicada/target/debug/deps/duplicada-1ff5f3c4cb255193.d
+++
@@ -1,5 +0,0 @@
-/home/david/Área de Trabalho/Programação /Rust/trilha_rust/GENERICOS/duplicada/target/debug/deps/duplicada-1ff5f3c4cb255193.d: src/main.rs
-
-/home/david/Área de Trabalho/Programação /Rust/trilha_rust/GENERICOS/duplicada/target/debug/deps/duplicada-1ff5f3c4cb255193: src/main.rs
-
-src/main.rs:
--- GENERICOS/duplicada/target/debug/duplicada.d
+++
@@ -1,1 +0,0 @@
-/home/david/Área\ de\ Trabalho/Programação\ /Rust/trilha_rust/GENERICOS/duplicada/target/debug/duplicada: /home/david/Área\ de\ Trabalho/Programação\ /Rust/trilha_rust/GENERICOS/duplicada/src/main.rs
