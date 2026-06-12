71fbeaf4a7e479b56238c577ef1e9cced71a4895
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-06-12 09:35:07 -0300

    Avançando na utilização de modulos

---
+++ MODULOs/modulo3/target/debug/deps/modulo3-0bbb7260b90e72f6.d
@@ -0,0 +1,8 @@
+/home/david/Área de Trabalho/Programação /Rust/trilha_rust/MODULOs/modulo3/target/debug/deps/modulo3-0bbb7260b90e72f6.d: src/main.rs src/animais.rs src/animais/mamiferos.rs src/animais/mamiferos/gato.rs
+
+/home/david/Área de Trabalho/Programação /Rust/trilha_rust/MODULOs/modulo3/target/debug/deps/modulo3-0bbb7260b90e72f6: src/main.rs src/animais.rs src/animais/mamiferos.rs src/animais/mamiferos/gato.rs
+
+src/main.rs:
+src/animais.rs:
+src/animais/mamiferos.rs:
+src/animais/mamiferos/gato.rs:
