2c82e89b650f7a86d49ce3f5cc9d8c7905ee90ef
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-07-16 10:09:57 -0300

    Exemplo Recursiva

---
+++ PROGAMAÇÃO FUNCIONAL/recursova/src/main.rs
@@ -0,0 +1,16 @@
+
+// === Exemplo de recursão =======
+fn soma_recursiva(n: i32) -> i32 {
+    if n == 0 {
+        0
+    } else {
+        n + soma_recursiva(n - 1)
+    }
+}
+
+fn main() {
+    let n = 5;
+    println!("A soma recursiva dos números até {} é: {}", n, soma_recursiva(n));
+}
+
+
