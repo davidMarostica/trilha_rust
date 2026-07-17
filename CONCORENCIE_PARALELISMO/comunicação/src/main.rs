fc2d62d42048d04f6da7636cb506ad3190bfe350
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2026-07-17 06:48:39 -0300

    comunicação entre tread

--- CONCORENCIE_PARALELISMO/comunicação/src/main.rs
+++ CONCORENCIE_PARALELISMO/comunicação/src/main.rs
@@ -1,3 +1,25 @@
+
+use std::sync::mpsc; // mpsc significa multiple producer, single consumer
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    // Cria um canal
+    let (tx, rx) = mpsc::channel(); // tx = envia o resultado, rx = recebe o resultado
+
+    // Cria uma nova thread
+    thread::spawn(move || {
+        let msg = String::from("Olá, Pessoal!");
+
+        thread::sleep(Duration::from_millis(1000));
+
+        // Envia uma mensagem pelo canal
+        tx.send(msg).unwrap();
+        // Note que `msg` não pode mais ser usado aqui, pois `send` toma a propriedade da mensagem
+    });
+
+
+    // Recebe a mensagem na thread principal
+    let received = rx.recv().unwrap(); // espera até a thread ser resolvida
+    println!("Mensagem recebida: {}", received);
 }
